d4e84eaed8838fd0f31e5ee67e8a007a0cf6fa0d
Delaram Rajaei <48606206+DelaramRajaei@users.noreply.github.com>
2022-02-18 11:11:19 +0330

    Add files via upload

---
+++ src/routes/Projects.js
@@ -0,0 +1,204 @@
+function Projects() {
+    return <>
+        <span className="fs-2 fw-bold text-uppercase">Academic Projects & Researches</span>
+        <hr />
+        <table className="table table-striped">
+            <tbody>
+                <tr>
+                    <td>
+                        <div className="d-flex justify-content-between">
+                            <span className="fw-bold">
+                                <a href="https://github.com/DelaramRajaei/Sprinkle">Sprinkle</a>
+                            </span>
+                            <span className="fw-bolder bg-secondary text-light px-3">
+                                Winter 2022
+                            </span>
+                        </div>
+                        <ul>
+                            <li>Estimating the time duration for watering soil</li>
+                            <li>Used fuzzy logic</li>
+                            <li>Language: Python</li>
+                        </ul>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div className="d-flex justify-content-between">
+                            <span className="fw-bold">
+                                <a href="https://github.com/DelaramRajaei/SnailJumper-master">SnailJumper-Master</a>
+                            </span>
+                            <span className="fw-bolder bg-secondary text-light px-3">
+                                Winter 2022
+                            </span>
+                        </div>
+                        <ul>
+                            <li>Language: Python</li>
+                            <li>Developed a game which can be played both manual and by neuroevolution algorithm. In this game there is an animal that should try to avoid obstacles and gain score.</li>
+                            <li>Used neuroevolution algorithm</li>
+                        </ul>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div className="d-flex justify-content-between">
+                            <span className="fw-bold">
+                                <a href="https://github.com/DelaramRajaei/SnailJumper-master">SnailJumper-Master</a>
+                            </span>
+                            <span className="fw-bolder bg-secondary text-light px-3">
+                                Winter 2022
+                            </span>
+                        </div>
+                        <ul>
+                            <li>Language: Python</li>
+                            <li>Developed a game which can be played both manual and by neuroevolution algorithm. In this game there is an animal that should try to avoid obstacles and gain score.</li>
+                            <li>Used neuroevolution algorithm</li>
+                        </ul>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div className="d-flex justify-content-between">
+                            <span className="fw-bold">
+                                <a href="https://github.com/DelaramRajaei/Inverted-pendulum">Invertedpendulum</a>
+                            </span>
+                            <span className="fw-bolder bg-secondary text-light px-3">
+                                Winter 2022
+                            </span>
+                        </div>
+
+                        <ul>
+                            <li>Language: Python</li>
+                            <li>Created a pendulum fixing on top of the cart in reverse. The cart can move either left or right with different speed in order to keep the pendulum fixed on top.</li>
+                            <li>Used fuzzy logic</li>
+                        </ul>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div className="d-flex justify-content-between">
+                            <span className="fw-bold">
+                                <a href="https://github.com/DelaramRajaei/Fruits-Classification">FruitsClassification</a>
+                            </span>
+                            <span className="fw-bolder bg-secondary text-light px-3">
+                                Winter 2022
+                            </span>
+                        </div>
+
+                        <ul>
+                            <li>Language: Python</li>
+                            <li>Created a machine recognizing three different fruits.</li>
+                            <li>Used neural network</li>
+                        </ul>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div className="d-flex justify-content-between">
+                            <span className="fw-bold">
+                                <a href="https://github.com/delaramrajaei/binary-puzzle">Binary puzzle</a>
+                            </span>
+                            <span className="fw-bolder bg-secondary text-light px-3">
+                                Spring 2021
+                            </span>
+                        </div>
+
+                        <ul>
+                            <li>Language: Python</li>
+                            <li>Filling a Binary puzzle according to some specific rules. Given N x N puzzle like sudoku but with binary numbers. There are some rules that should be applied before solving each puzzle. The machine can also recognize unsolvable puzzles.</li>
+                        </ul>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div className="d-flex justify-content-between">
+                            <span className="fw-bold">
+                                <a href="https://github.com/DelaramRajaei/IDS">Bringing butter</a>
+                            </span>
+                            <span className="fw-bolder bg-secondary text-light px-3">
+                                Spring 2021
+                            </span>
+                        </div>
+
+                        <ul>
+                            <li>Language: Java</li>
+                            <li>Developed a robot choosing the optimal way in order to bring butter to a person on a table. There is a table full of obstacles, n butters (at least one) and n people who are waiting around the table to be served by the butter. The robot has the responsibility to push the butter to a person with less possible moves. It can move in 4 ways, up, down, left and right.</li>
+                            <li>Implemented iterative deepening depth-first search (IDS) algorithm</li>
+                        </ul>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div className="d-flex justify-content-between">
+                            <span className="fw-bold">
+                                <a href="https://github.com/hedzd/sentiment-analysis">SentimentAnalysis</a>
+                            </span>
+                            <span className="fw-bolder bg-secondary text-light px-3">
+                                Spring 2021
+                            </span>
+                        </div>
+
+                        <ul>
+                            <li>Language: Python</li>
+                            <li>Natural language Processing (NLP)</li>
+                            <li>Deciding if a comment is negative or positive.</li>
+                        </ul>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div className="d-flex justify-content-between">
+                            <span className="fw-bold">
+                                <a href="https://github.com/delaramrajaei/information-retrieval">Information-RetrievalProject</a>
+                            </span>
+                            <span className="fw-bolder bg-secondary text-light px-3">
+                                Spring 2021
+                            </span>
+                        </div>
+
+                        <ul>
+                            <li>Language: Java</li>
+                            <li>Created a search engine.</li>
+                            <li>Used inverted index, tf-idf, max heap in three sections</li>
+                        </ul>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div className="d-flex justify-content-between">
+                            <span className="fw-bold">
+                                <a href="https://github.com/DelaramRajaei/Theory-Of-Machines-and-Languages">Theory of machines and languages</a>
+                            </span>
+                            <span className="fw-bolder bg-secondary text-light px-3">
+                                Summer 2020
+                            </span>
+                        </div>
+
+                        <ul>
+                            <li>Language: Java</li>
+                            <li>Deciding whether the given state is accepted by DFA or NDFA algorithm</li>
+                        </ul>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div className="d-flex justify-content-between">
+                            <span className="fw-bold">
+                                <a href="https://github.com/DelaramRajaei/Jpotify">Jpotify</a>
+                            </span>
+                            <span className="fw-bolder bg-secondary text-light px-3">
+                                Summer 2019
+                            </span>
+                        </div>
+
+                        <ul>
+                            <li>Language: Java</li>
+                            <li>Implemented Spotify. Developed an application for listening to music and can share the name of the song with your friends.</li>
+                        </ul>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </>
+}
+
+export default Projects;
