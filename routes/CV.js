e1881561e433c442f42f3c72a0c0aa6103476d13
Delaram Rajaei <48606206+DelaramRajaei@users.noreply.github.com>
2022-02-21 14:46:50 +0330

    Add files via upload

---
+++ routes/CV.js
@@ -0,0 +1,9 @@
+function CV() {
+    return <>
+        <a className="btn btn-outline-primary btn-lg" target="_blank" href="/Delaram Rajaei_CV.pdf">
+            <i class="bi bi-filetype-pdf pe-3"></i>Download
+        </a>
+    </>
+}
+
+export default CV;
